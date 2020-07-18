4ebdfa4cc393d2350aa0d26a2553886ec49320c4
vlam450 <z5264471@ad.unsw.edu.au>
2020-07-18 21:14:11 +1000

    fixed sortListing after SQL implementation

--- backend/listing.py
+++ backend/listing.py
@@ -1,9 +1,8 @@
-import pickle
-from opencage.geocoder import OpenCageGeocode
+from geopy.geocoders import Nominatim
+from geopy import distance
 from backend import database
 import sqlite3
 
-openCageAPIKey = '314a7cb65c1c4dcfb675e5da37bc26bc'
 
 def getTitle(id):
     conn = sqlite3.connect('helpout.db')
@@ -54,33 +53,40 @@ def nextID():
     return id
 
 
-def sortListing(id):
+def sortListing(num, location):
+
     closeListings = []
 
-    db = database.readListingsDatabase()
-    geocoder = OpenCageGeocode(openCageAPIKey)
+    geolocator = Nominatim(user_agent= 'myGeocoder')
 
-    currentuburb = str(db[id]['Location'])
-    currentLoc = geocoder.geocode(suburb)
-    currentLoc_lat = currentLoc[0]['geometry']['lat']
-    currentLoc_lng = currentLoc[0]['geometry']['lng']
+    currentLoc = geolocator.geocode(location)
+    currentLoc_lat = currentLoc.latitude
+    currentLoc_lng = currentLoc.longitude
 
-    for listing in db:
-        suburb = str(db[listing]['Location'])  
-        subCheck = geocoder.geocode(suburb, countrycode = au)
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    length = cursor.execute("SELECT COUNT(*) FROM listings")
 
-        subCheck_lat = subCheck[0]['geometry']['lat']
-        subCheck_lng = subCheck[0]['geometry']['lng']
+    for i in range(length):
+        result = cursor.execute("SELECT person FROM listings WHERE location="+str(location))
+        suburb = result.fetchone()[0]
 
-        distance = geodesic((currentLoc_lat,currentLoc_lng), (subCheck_lat,subCheck_lng)).kilometers
+        subCheck = geolocator.geocode(suburb)
 
-        listing = (suburb, distance)
-        closeListings.append(listing)
+        subCheck_lat = subCheck.latitude
+        subCheck_lng = subCheck.longitude
+
+        dist = distance.distance((currentLoc_lat,currentLoc_lng), (subCheck_lat,subCheck_lng)).km
+
+        subDist = (subCheck, dist)
+        closeListings.append(subDist)
 
     closeListings = closeListings.sort(key = lambda x: x[1]) # sort by distance
     tempList = []
 
-    for i in range(len(closeListings)):
+    conn.close()
+
+    for i in range(num):
         tempList.append(closeListings[i][1])
 
     return tempList
