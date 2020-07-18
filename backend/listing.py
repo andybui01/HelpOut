60f7f25fd6b14fb04c36f8fe18635d09056696ec
andybui01 <andy.bui2001@gmail.com>
2020-07-18 20:49:15 +1000

    change to sqlite3

--- backend/listing.py
+++ backend/listing.py
@@ -1,12 +1,97 @@
+import pickle
+from opencage.geocoder import OpenCageGeocode
+from backend import database
+import sqlite3
+
+openCageAPIKey = '314a7cb65c1c4dcfb675e5da37bc26bc'
+
 def getTitle(id):
-    return "Victor is gay"
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT title FROM listings WHERE id="+str(id))
+    title = result.fetchone()[0]
+    conn.close()
+    return title
 
 def getDesc(id):
-    return "Desc"
-
-def getCommitment(id):
-    return "Commitment"
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT desc FROM listings WHERE id="+str(id))
+    desc = result.fetchone()[0]
+    conn.close()
+    return desc
 
 def getLocation(id):
-    return "Location"
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT location FROM listings WHERE id="+str(id))
+    location = result.fetchone()[0]
+    conn.close()
+    return location
 
+def getCommitment(id):
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT commitment FROM listings WHERE id="+str(id))
+    commitment = result.fetchone()[0]
+    conn.close()
+    return commitment
+
+def getPerson(id):
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT person FROM listings WHERE id="+str(id))
+    person = result.fetchone()[0]
+    conn.close()
+    return person
+
+def nextID():
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT COUNT(*) FROM listings")
+    id = int(result.fetchone()[0])
+    conn.close()
+    return id
+
+
+def sortListing(id):
+    closeListings = []
+
+    db = database.readListingsDatabase()
+    geocoder = OpenCageGeocode(openCageAPIKey)
+
+    currentuburb = str(db[id]['Location'])
+    currentLoc = geocoder.geocode(suburb)
+    currentLoc_lat = currentLoc[0]['geometry']['lat']
+    currentLoc_lng = currentLoc[0]['geometry']['lng']
+
+    for listing in db:
+        suburb = str(db[listing]['Location'])  
+        subCheck = geocoder.geocode(suburb, countrycode = au)
+
+        subCheck_lat = subCheck[0]['geometry']['lat']
+        subCheck_lng = subCheck[0]['geometry']['lng']
+
+        distance = geodesic((currentLoc_lat,currentLoc_lng), (subCheck_lat,subCheck_lng)).kilometers
+
+        listing = (suburb, distance)
+        closeListings.append(listing)
+
+    closeListings = closeListings.sort(key = lambda x: x[1]) # sort by distance
+    tempList = []
+
+    for i in range(len(closeListings)):
+        tempList.append(closeListings[i][1])
+
+    return tempList
+
+
+def createListing(personID = None, title = None, desc = None, commitment = None, location = None):
+    conn = sqlite3.connect('helpout.db')
+    cursor = conn.cursor()
+    id = nextID()
+    print(title, desc, commitment)
+    items = (str(id), title, desc, commitment, location, personID, str(-1), 0)
+    cursor.execute("INSERT INTO listings VALUES (?,?,?,?,?,?,?,?)", items)
+    conn.commit()
+    conn.close()
--- server.py
+++ server.py
@@ -7,40 +7,41 @@ app = Flask(__name__)
 """ Volunteer """
 @app.route('/volunteer/name', methods=["GET"])
 def volunteerName():
-	return dumps(volunteer.getName(request.form.get('volunteer_id')))
+	return dumps(volunteer.getName(request.args.get('volunteer_id')))
 
 @app.route('/volunteer/contact', methods=["GET"])
 def volunteerContact():
-	return dumps(volunteer.getContact(request.form.get('volunteer_id')))
+	return dumps(volunteer.getContact(request.args.get('volunteer_id')))
 
 @app.route('/volunteer/completed', methods=["GET"])
 def volunteerCompleted():
-	return dumps(volunteer.getNumberCompleted(request.form.get('volunteer_id')))
+	return dumps(volunteer.getNumberCompleted(request.args.get('volunteer_id')))
 
 @app.route('/volunteer/badges', methods=["GET"])
 def volunteerBadges():
-	return dumps(volunteer.getBadges(request.form.get('volunteer_id')))
+	return dumps(volunteer.getBadges(request.args.get('volunteer_id')))
 
 """ Listings """
 @app.route('/listing/title', methods=["GET"])
 def listingTitle():
-	return dumps(listing.getTitle(request.form.get('listing_id')))
+	return dumps(listing.getTitle(request.args.get('listing_id')))
 	
 @app.route('/listing/desc', methods=["GET"])
 def listingDesc():
-	return dumps(listing.getDesc(request.form.get('listing_id')))
+	return dumps(listing.getDesc(request.args.get('listing_id')))
 
 @app.route('/listing/commitment', methods=["GET"])
 def listingCommitment():
-	return dumps(listing.getCommitment(request.form.get('listing_id')))
+	return dumps(listing.getCommitment(request.args.get('listing_id')))
 
 @app.route('/listing/location', methods=["GET"])
 def listingLocation():
-	return dumps(listing.getLocation(request.form.get('listing_location')))
+	return dumps(listing.getLocation(request.args.get('listing_location')))
 
 @app.route('/listing/create', methods=["POST"])
 def listingCreate():
 	return dumps(listing.createListing(
+		request.form.get('personID'),
 		request.form.get('title'),
 		request.form.get('desc'),
 		request.form.get('commitment'),
